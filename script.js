e4049e305670862e58c1fc363cdeeccc689b0778
[mercadohectordw] <[mercadohectordw@gmail.com]>
2023-02-10 12:18:08 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const mostrarYOcultarElementos = () => {
+  let element = document.querySelector('#inicio');
+  let position = element.getBoundingClientRect();
+  let waves = document.getElementById('waves').style;
+
+  if(position.bottom < 0 && waves.opacity == 1) {
+    waves.opacity = 0;
+  }
+  if(position.bottom > 0 && waves.opacity == 0) {
+    waves.opacity = 1;
+  }
+}
+
+mostrarYOcultarElementos();
+
+window.addEventListener('scroll', function() {
+  mostrarYOcultarElementos();
+});
