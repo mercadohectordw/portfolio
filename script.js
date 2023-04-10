afd9c20b14d54ec7bdd2850d740552d6dc59fc5a
[mercadohectordw] <[mercadohectordw@gmail.com]>
2023-04-10 00:59:32 -0300

    cambio radical, version de escritorio

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const mostrarYOcultarElementos = () => {
-  let element1 = document.querySelector('.ini__avatar');
-  let element2 = document.querySelector('#contacto');
-  let position1 = element1.getBoundingClientRect();
-  let position2 = element2.getBoundingClientRect();
+  let inicio = document.querySelector('#inicio').getBoundingClientRect();
+  let contacto = document.querySelector('#contacto').getBoundingClientRect();
   let waves = document.getElementById('waves').style;
 
-  if((position1.bottom < 0 || position2.top != 0 ) && waves.opacity == 1) {
+  if((inicio.bottom < 0 || contacto.top != 0 ) && waves.opacity == 1) {
     waves.opacity = 0;
   }
-  if((position1.bottom > 0 || position2.top == 0) && waves.opacity == 0) {
+  if((inicio.top == 0 || contacto.top == 0) && waves.opacity == 0) {
     waves.opacity = 1;
   }
 }
